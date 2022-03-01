56aa00cd716132644de9bea5e42f042ab9684da6
Nithya-24 <nithyaramadoz@gmail.com>
2022-03-01 22:31:57 +0530

    Find Greatest String Program Added

--- src/practice_Problem/FindGreaterNo.java
+++ src/practice_Problem/FindGreaterNo.java
@@ -15,6 +15,9 @@ public class FindGreaterNo{
 	}
 
 	public static void main(String[] args)  {
-		System.out.println("Maximum of 3 Nos:" + maximum (8, 4, 5));
+		System.out.println("Maximum of 3 Nos: " + maximum (8, 4, 5));
+		System.out.println("Maximum of 3 Floating Nos: " + maximum (8.5, 4.5, 5.5));
+		System.out.println("Maximum of 3 Strings: " + maximum ("Apple", "Peach", "Banana"));
+		
 	}
 }
